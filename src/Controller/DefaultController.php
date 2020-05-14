647c66b1c27028ab65d92ecdf682d890505915ee
n-tanoDC <nicolas.tano42@gmail.com>
2020-05-14 12:02:24 +0200

    Optimisation du code + création d'un controller pour le menu déroulant 'Catégories'

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Project;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,5 +24,10 @@ class DefaultController extends AbstractController
             'projects' => $projects,
             'categories' => $categories
         ]);
+    }
+
+    public function searchForm(CategoryRepository $categoryRepository)
+    {
+        return $this->render("default/_search_form.html.twig", ["categories" => $categoryRepository->findAll()]);
     }
 }
