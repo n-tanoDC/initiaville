8fde42bfea3b27c207db0b538fa578ea5584a0b9
n-tanoDC <nicolas.tano42@gmail.com>
2020-05-14 10:50:53 +0200

    Mise en forme de project-new et project-show

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -2,8 +2,17 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
+use App\Entity\City;
 use App\Entity\Project;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +21,35 @@ class ProjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('picture')
-            ->add('cost')
-            ->add('excerpt')
-            ->add('description')
-            ->add('createdAt')
-            ->add('user')
-            ->add('city')
-            ->add('categories')
+            ->add('title', TextType::class, [
+                'label' => 'Titre ',
+                'attr' => [
+                    'placeholder' => 'Titre'
+                ]
+            ])
+            ->add('picture', FileType::class, [
+                'label' => 'Image',
+                'mapped' => false
+            ])
+            ->add('cost', IntegerType::class, [
+                'label' => 'Coût du projet'
+            ])
+            ->add('excerpt', TextType::class, [
+                'label' => 'Description courte'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('categories', EntityType::class, [
+                'label' => 'Catégorie(s)',
+                'class' => Category::class,
+                "multiple" => true,
+                'expanded' => true
+            ])
+            ->add('city' , EntityType::class, [
+                'class' => City::class,
+                'label' => 'Ville',
+            ])
         ;
     }
 
